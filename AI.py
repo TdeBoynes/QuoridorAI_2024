b44632a793690d172672a734fb46b9c6eab41a0b
Timothee de Boynes <72024605+TdeBoynes@users.noreply.github.com>
2024-07-18 11:55:19 +0900

    update(wall): Now place walls in a smarter way

--- AI.py
+++ AI.py
@@ -4,36 +4,39 @@ from random import choices
 from random import choice
 
 class AI:
-    updatePath = True
+    updatePath = {'P1': True, 'P2': True}
     goal = {'P1': (0,2), 'P2': (4,2)}
     paths = {'P1': [], 'P2': []}
+    enemy = {'P1': 'P2', 'P2': 'P1'}
 
     def get_move(self, player, game):
-        if (self.updatePath):
+        print("\nPLAYER: ", player)
+        if (self.updatePath[player]):
             self.paths[player] = game.extract_path(game.player_positions[player], self.goal[player])
-            self.updatePath = False
+            self.updatePath[player] = False
             
-
         action = choices(['move', 'wall'], weights=[70, 30], k=1)[0]
-        print("\nPLAYER: ", player)    
         if action == 'move':
             return self.move(player, game)
             
         elif action == 'wall':
-            self.updatePath = True
-            return self.place_wall(game)
+            self.updatePath['P1'] = True
+            self.updatePath['P2'] = True
+            return self.place_wall(player, game, game.player_positions[self.enemy[player]])
     
     def move(self, player, game):
         move = self.paths[player].pop(0)
         return move
-        # legal_moves = game.get_legal_moves()
-        # return choice(legal_moves)
-        # if legal_moves:
-        # else:
-        #     return None  # No legal moves available
     
-    def place_wall(self, game):
+    
+    def place_wall(self, player, game, enemy_position):
         legal_moves = game.get_legal_moves()
-        return choice(legal_moves)
-        # Implement logic to place a wall
-        pass
+        
+        wall_moves = [move for move in legal_moves if move[0] in ('H', 'V')]
+        
+        wall_moves.sort(key=lambda move: (abs(move[1] - enemy_position[0]) + abs(move[2] - enemy_position[1]), move[0] == 'V'))
+        
+        for move in wall_moves:
+            if move[0] == 'H':
+                return move
+        return wall_moves[0] if wall_moves else self.move(player, game)
